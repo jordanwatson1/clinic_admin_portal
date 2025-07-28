dc6247501bd75a1c9684b6784fc9829c2b9d34dd
Jordan Watson <jordanwatson915@gmail.com>
2025-07-28 08:48:45 -0700

    Update schema.rb after creating appointments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_26_202540) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_26_210903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "patient_id", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +32,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_26_202540) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_patients_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +47,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_26_202540) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "appointments", "patients"
+  add_foreign_key "patients", "users"
 end
