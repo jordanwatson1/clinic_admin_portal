a976d6254f37f84ae8110134e69a903521fe4b40
Jordan Watson <jordanwatson915@gmail.com>
2025-07-28 08:46:00 -0700

    Set up associations between Patient and Appointment models

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,9 @@
 class Patient < ApplicationRecord
+  has_many :appointments, dependent: :destroy
+  belongs_to :user  # For multi-user support
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+  
 end
