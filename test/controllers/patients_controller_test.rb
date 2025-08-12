9777b39ae2734ccc07bb39ab25f10fad83cb9fba
Jordan Watson <jordanwatson915@gmail.com>
2025-08-12 10:08:40 -0700

    test(patients_controller): add authenticated CRUD + ownership checks; fix expectations

--- test/controllers/patients_controller_test.rb
+++ test/controllers/patients_controller_test.rb
@@ -1,8 +1,14 @@
+# test/controllers/patients_controller_test.rb
 require "test_helper"
 
 class PatientsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = users(:one)
+    sign_in @user
+
     @patient = patients(:one)
+    # Ensure the patient we exercise actually belongs to the signed-in user
+    @patient.update!(user: @user) if @patient.user != @user
   end
 
   test "should get index" do
@@ -16,11 +22,18 @@ class PatientsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create patient" do
-    assert_difference("Patient.count") do
-      post patients_url, params: { patient: { dob: @patient.dob, email: @patient.email, first_name: @patient.first_name, last_name: @patient.last_name, phone: @patient.phone } }
+    assert_difference("Patient.where(user: @user).count") do
+      post patients_url, params: {
+        patient: {
+          first_name: "Pat",
+          last_name:  "New",
+          email:      "pat.new@example.com",
+          phone:      "555-1234",
+          dob:        "1990-01-01"
+        }
+      }
     end
-
-    assert_redirected_to patient_url(Patient.last)
+    assert_redirected_to patient_url(Patient.order(:created_at).last)
   end
 
   test "should show patient" do
@@ -34,15 +47,14 @@ class PatientsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update patient" do
-    patch patient_url(@patient), params: { patient: { dob: @patient.dob, email: @patient.email, first_name: @patient.first_name, last_name: @patient.last_name, phone: @patient.phone } }
+    patch patient_url(@patient), params: { patient: { first_name: "Updated" } }
     assert_redirected_to patient_url(@patient)
   end
 
   test "should destroy patient" do
-    assert_difference("Patient.count", -1) do
+    assert_difference("Patient.where(user: @user).count", -1) do
       delete patient_url(@patient)
     end
-
     assert_redirected_to patients_url
   end
 end
